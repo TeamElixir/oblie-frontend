950288a6d112984d770808cdeb1b999c137a8fe3
Menuka Warushavithana <menuka.14@cse.mrt.ac.lk>
2018-02-28 20:47:25 +0530

    feat: change diagram to orgChart

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+console.log("Hello World");
 var $ = go.GraphObject.make;
 var myDiagram =
     $(go.Diagram, "myDiagramDiv",
@@ -9,24 +10,44 @@ var myDiagram =
         });
 
 // the template we defined earlier
+// if(myDiagram.nodeDataArray["type"] == "red") {
+//     console.log("red");
+// }
+
+// if(myDiagram.nodeDataArray.type == "red") {
+//     console.log("red");
+// }
+
+console.log("Go Node", go.Node);
 myDiagram.nodeTemplate =
-    $(go.Node, "Horizontal",
-        {background: "#44CCFF"},
-        // $(go.Picture,
-        //     {margin: 10, width: 50, height: 50, background: "red"},
-        //     new go.Binding("source")),
+    $(go.Node, "Horizontal", {background: "#44CCFF"},
+        $(go.Picture, {margin: 10, width: 50, height: 50, background: "red"}, new go.Binding("source")),
         $(go.TextBlock, "Default Text",
-            {margin: 12, width: 300, stroke: "white", font: "bold 16px sans-serif"},
+            {
+                margin: 12,
+                width: 300,
+                // stroke: "white",
+                stroke: ("type"),
+                font: "bold 16px sans-serif"
+            },
+            // new go.Binding("text", "type"))
             new go.Binding("text", "name"))
     );
 
 var model = $(go.TreeModel);
 
 var url = "http://localhost:9000/arguments.json";
-jQuery.getJSON(url, function(data){
+jQuery.getJSON(url, function (data) {
     model.nodeDataArray = data;
     myDiagram.model = model;
+    // console.log(data[7]["type"]);
+    var i = 0;
+    for (; i < Object.keys(model.nodeDataArray).length; i++) {
+        console.log("type of ", i);
+        console.log(data[i]["type"]);
+    }
 });
+
 
 // jQuery.ajax({
 //     type: 'GET',
